917d03f55d0ca4096b151f362e6e91656a0d6d20
Fynn8962 <142886149+Fynn8962@users.noreply.github.com>
2024-01-30 07:44:20 +0100

    Add files via upload

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Linq.Expressions;
 using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
@@ -7,26 +8,35 @@ namespace Game
 {
     internal class Program
     {
+        // Nullable<timeSpan> sagt, das die Timespan auch Null sein darf. Kurzschreibweise ist Timespan?
 
+        public static Nullable<TimeSpan> reactiontime;
         public static bool repeat;
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Reaction Time Game\n");
-            Console.WriteLine("Difficulty options:\n Easy.....[1]\n Medium...[2]\n Hard.....[3]\n Extreme..[4]");
+            
+            
             difficulty();
         }
         private static void difficulty()
         {
+            Console.Clear();
+            Console.WriteLine("Reaction Time Game\n");
+            Console.WriteLine("Difficulty options:\n " +
+                "1000ms..Easy.....[1]\n " +
+                "500ms...Medium...[2]\n " +
+                "250ms...Hard.....[3]\n " +
+                "125ms...Extreme..[4]");
+          
             try
             {
-                 repeat = false;
-                while (!repeat) 
+                repeat = false;
+                while (!repeat)
                 {
                     Console.CursorVisible = false;
 
-                    // Nullable<timeSpan> sagt, das die Timespan auch Null sein darf. Kurzschreibweise ist Timespan?
-                    Nullable<TimeSpan> reactiontime = null;
+                   
+                    reactiontime = null;
                     while (reactiontime == null)
                     {
                         switch (Console.ReadKey(true).Key)
@@ -37,9 +47,10 @@ namespace Game
                             case ConsoleKey.D4: reactiontime = TimeSpan.FromMilliseconds(125); break;
                         }
                     }
-
+                    
 
                     Console.Clear();
+                    Console.CursorVisible = false;
                     TimeSpan Signal = TimeSpan.FromMilliseconds(Random.Shared.Next(2000, 5000));
                     Console.WriteLine("wait");
                     Stopwatch stopwatch = new();
@@ -49,60 +60,59 @@ namespace Game
                     {
                         if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar)
                         {
-
-                            tooFastFunktion();
-
+                            Console.Clear() ;
+                            tooFast = true;
+                            Console.WriteLine("too Fast!\n");
+                            restart();
+                           
                         }
                     }
 
-                    Console.Clear();
-                    stopwatch.Restart();
-                    Console.WriteLine("Click");
-                    bool tooSlow = true;
+                    bool tooSlow = false;
                     TimeSpan reactionTimeFinal = default;
-                    
+                    stopwatch.Restart();
+                    bool hit = false;
+                    if (tooFast == false)
+                    {
+                        Console.Clear();
+                        stopwatch.Restart();
+                        Console.WriteLine("Click");
+                        
+                    }
+
                     while (tooFast == false && stopwatch.Elapsed < reactiontime)
                     {
-                        if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar && tooSlow)
+                        if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar)
                         {
-                            tooSlow = false;
+                            stopwatch.Stop();
                             reactionTimeFinal = stopwatch.Elapsed;
-
+                            Console.Clear();
+                            tooFast = false;
+                            tooSlow = false;
+                            hit = true;
+                            double millisecond = reactionTimeFinal.TotalMilliseconds;
+                            int roundedmilliseconds = (int)Math.Round(millisecond);
+                            Console.WriteLine("Nice!\n");
+                            Console.WriteLine("Reactiontime: " + roundedmilliseconds + " Milliseconds" + "\n");
+                            restart();
                         }
-
+                        else if (stopwatch.Elapsed > reactiontime && hit == false)
+                        {
+                            Console.Clear();
+                            tooSlow = true;
+                           
+                            
+                        }
+                        
                     }
-                    double millisecond = reactionTimeFinal.TotalMilliseconds;
-                    int roundedmilliseconds = (int)Math.Round(millisecond);
-                    Console.WriteLine("Reactiontime: " + roundedmilliseconds + " Milliseconds" + "\n");
-                    Console.WriteLine("Restart? [y: 1|n: 2]");
-
-
-                    switch (Console.ReadKey(true).Key)
-
-                    {
-                        case ConsoleKey.D1:
-                            repeat = false;
-                            break;
-                        case ConsoleKey.D2:
-                            Stopgame();
-                            break;
-                    }
+                   
+                    
 
                     if (tooSlow == true)
                     {
                         Console.Clear();
                         Console.WriteLine("Too Slow!\n");
-                        Console.WriteLine("Restart? [y: 1|n: 2]");
-
-                       
-                        switch (Console.ReadKey(true).Key)
-
-                        {
-                            case ConsoleKey.D1: repeat = false;
-                                break; 
-                            case ConsoleKey.D2: Stopgame(); 
-                                break;
-                        }
+                        restart();
 
 
 
@@ -117,27 +127,65 @@ namespace Game
             {
                 Console.Clear();
                 Console.WriteLine("ERROR");
-            } 
-        
-        }   
+            }
+
+        }
 
         private static void tooFastFunktion()
         {
+
+            
             Console.Clear();
             Console.WriteLine("too Fast!");
-            
-            
+            repeat = true;
+
+
+
+
+
         }
         private static void Stopgame()
         {
             Console.Clear();
             repeat = true;
-            
-            
-            
-        }
-        
 
+
+
+        }
+
+        private static void restart()
+        {
+            Console.WriteLine("-------------------\nRestart? yes -> 1\n \t no --> 2\n-------------------");
+            reactiontime = null;
+
+            string retry = "yes";
+
+            do
+            {
+                switch (Console.ReadKey(true).Key)
+
+                {
+                    case ConsoleKey.D1:
+                        
+                        difficulty();
+                        break;
+                    case ConsoleKey.D2:
+                        
+                        Stopgame();
+                        break;
+                    case ConsoleKey Spacebar:
+                        retry = "no";
+                        Console.Clear();
+                        restart();
+                        break;
+
+                }
+            }
+            while (retry != "yes");
+
+            
+
+        }
 
     }
 }
